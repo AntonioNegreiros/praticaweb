42ee21e895a968dfa5d31839ebab53847fb64a48
Antônio Negreiros <antonio.negreiros01@gmail.com>
2024-09-11 21:33:35 -0300

    revisao1

--- servico.php
+++ servico.php
@@ -6,7 +6,21 @@ include 'includes/header.php';
 <main>
     <div>
         <h1>Serviços</h1>
-        <p>Acessar Serviços</p>
+        <img src="assets/images/servicos.jpg" class="img-fluid">
+        <p>Nossos Serviços:</p>
+        <br>
+
+        <ol>
+            <li> 🖥️ Criação de Sites </li>
+            <br>
+            <li> 🔖 Campanhas Publicitárias <li>
+            <br>
+            <li> ®️ Registro de Marca <li>
+            <br>
+            <li> ♻️ Reciclamos seu material <li>
+
+        </ol>
+        
     </div>
 </main>
  
